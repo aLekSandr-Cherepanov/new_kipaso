d310450dac5999275a636cf0d87752186554d351
Alexander Cherepanov <super.sany96@yandex.ru>
2023-02-22 14:30:35 +0300

    мелкие правки

---
+++ storage/cache/template/8e/8edc1db9f2539de769b97bbb9eb43ed2df5235289233d471cd4fd2cfeb9680a9.php
@@ -0,0 +1,66 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* nice/template/common/home.twig */
+class __TwigTemplate_39ecca4fa504825369f83e4586d398a244b6b9558d4a53d6d8ecabe0b86c4be2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo ($context["header"] ?? null);
+        echo "
+<div id=\"common-home\" class=\"container\">
+  
+</div>
+";
+        // line 5
+        echo ($context["footer"] ?? null);
+    }
+
+    public function getTemplateName()
+    {
+        return "nice/template/common/home.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 5,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "nice/template/common/home.twig", "");
+    }
+}
